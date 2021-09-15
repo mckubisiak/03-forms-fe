dcb1ab374a67b7154a6fb19e50e7d49b967c28b3
Kubi Kubisiak <mckubisiak@gmail.com>
2021-09-14 19:00:31 -0700

    input and button on screen

--- src/components/app/containers/NewsContainer.jsx
+++ src/components/app/containers/NewsContainer.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import { fetchNews } from '../../../services/Newsapi';
+import Articlelist from '../../articles/Articlelist';
+import Search from '../../articles/Search';
 
 export default class NewContainer extends Component {
   state = {
-    loading: true,
-    articles: []
+    loading: false,
+    articles: [],
+    searchWord: 'Search for news here!',
+  };
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = async () => {
+    const response = 
+    await fetchNews(this.state.searchWord);
+    this.setState({ articles: response });
   }
 
   render() {
-    return <p>Time for the News</p>;
+    return (
+      <>
+        <Search
+          searchWord={this.state.searchWord}
+          onChange={this.handleChange}
+          submit={this.handleSubmit}
+        />
+        <Articlelist articles={this.state.articles}/>
+      </>
+    );
   }
 }
---
+++ src/components/articles/Search.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Search = ({ searchWord, onChange, submit }) => {
+  return (
+    <>
+      <input
+        aria-label="keyword search"
+        type="text"
+        name="searchWord"
+        value={searchWord}
+        onChange={onChange}
+      />
+
+      <button onClick={submit}>Search the news!</button>
+    </>
+  );
+};
+
+Search.propTypes = {
+  searchWord: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  submit: PropTypes.func.isRequired,
+};
+
+export default Search;
